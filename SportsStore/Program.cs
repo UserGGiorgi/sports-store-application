dafb30c7ce4abc3492b164a34b7b474eaf101fff
Giorgi Abesadze <abesdzegiorgi@gmail.com>
2024-12-09 13:52:42 +0400

    Add shopping cart functionality.

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
 builder.Services.AddDbContext<StoreDbContext>(opts =>
 {
      opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
@@ -14,6 +17,7 @@ builder.Services.AddDbContext<StoreDbContext>(opts =>
 var app = builder.Build();
 
 app.UseStaticFiles();
+app.UseSession();
 
 app.MapControllerRoute(
     name: "pagination",
@@ -29,6 +33,11 @@ app.MapControllerRoute(
     name: "category",
     pattern: "Products/{category}",
     defaults: new { Controller = "Home", action = "Index", productPage = 1 });
+
+app.MapControllerRoute(
+    name: "shoppingCart",
+    pattern: "Cart",
+    defaults: new { Controller = "Cart", action = "Index" });
 
 app.MapControllerRoute(
     name: "default",
