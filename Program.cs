c52883a2cb841159253ac43292456fe9e49f7e3a
Abesdzegiorgi <abesdzegiorgi@gmail.com>
2024-12-14 23:01:35 +0400

    fix directory

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ using SportsStore.Models.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddRazorRuntimeCompilation();
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 builder.Services.AddScoped<IOrderRepository, EFOrderRepository>();
 builder.Services.AddDistributedMemoryCache();
